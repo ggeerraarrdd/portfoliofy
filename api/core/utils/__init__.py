231a1c63b5210d664749c1e335784cab1d4e3785
ggeerraarrdd <pasetes@gmail.com>
2025-03-08 09:54:17 -0600

    Fix import error

--- api/core/utils/__init__.py
+++ api/core/utils/__init__.py
@@ -19,7 +19,6 @@ Functions:
 
 from .utils import get_screenshot
 from .utils import get_screenshot_full
-from .utils import get_screenshot_full_chrome
 from .utils import get_base
 from .utils import get_overlay
 from .utils import get_final_temp
@@ -31,7 +30,6 @@ from .utils import get_mime
 # Define what should be available when using "from .utils import *"
 __all__ = ['get_screenshot',
            'get_screenshot_full',
-           'get_screenshot_full_chrome',
            'get_base',
            'get_overlay',
            'get_final_temp',
