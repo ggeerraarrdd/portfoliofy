f4c9520a5f118ecb8b127296b0d8a9585dccb487
ggeerraarrdd <pasetes@gmail.com>
2025-03-11 01:04:56 -0500

    Test base_fill_color

--- api/domain/schemas/schemas.py
+++ api/domain/schemas/schemas.py
@@ -59,6 +59,14 @@ class PortfoliofyRequest(BaseModel):
     format: str = Field("png", pattern="^(png|jpe?g|bmp|tiff|pdf|mp4)$", description="Output file format")
     doc_pad_h: conint(gt=0, lt=1001) = Field(300, description="Padding value for horizontal dimension") # type: ignore[reportInvalidTypeForm]
     doc_pad_v: conint(gt=0, lt=1001) = Field(200, description="Padding value for vertical dimension") # type: ignore[reportInvalidTypeForm]
+
     doc_fill_color: constr(pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") = Field("#ffffff", description="RGB hexadecimal value") # type: ignore[reportInvalidTypeForm]
+
     base_stroke_color: constr(pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") = Field("#23445d", description="RGB hexadecimal value") # type: ignore[reportInvalidTypeForm]
-    base_fill_color: constr(pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") = Field("#bac8d3", description="RGB hexadecimal value") # type: ignore[reportInvalidTypeForm]
+    # base_fill_color: constr(pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") = Field("#bac8d3", description="RGB hexadecimal value") # type: ignore[reportInvalidTypeForm]
+
+    base_fill_color: str = Field(
+        "#bac8d3",
+        description="RGB hexadecimal value",
+        pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+    )
