f2e762d6ee07415f38c28ed19b7855a94656885f
ggeerraarrdd <pasetes@gmail.com>
2025-03-11 01:17:52 -0500

    Fix syntax errors

--- api/domain/schemas/schemas.py
+++ api/domain/schemas/schemas.py
@@ -2,7 +2,7 @@
 Pydantic model for validating and processing Portfoliofy screenshot requests.
 """
 
-from pydantic import BaseModel, HttpUrl, constr, conint, Field
+from pydantic import BaseModel, HttpUrl, conint, Field
 
 
 
@@ -60,10 +60,17 @@ class PortfoliofyRequest(BaseModel):
     doc_pad_h: conint(gt=0, lt=1001) = Field(300, description="Padding value for horizontal dimension") # type: ignore[reportInvalidTypeForm]
     doc_pad_v: conint(gt=0, lt=1001) = Field(200, description="Padding value for vertical dimension") # type: ignore[reportInvalidTypeForm]
 
-    doc_fill_color: constr(pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") = Field("#ffffff", description="RGB hexadecimal value") # type: ignore[reportInvalidTypeForm]
+    doc_fill_color: str = Field(
+        "#ffffff",
+        description="RGB hexadecimal value",
+        pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+    )
 
-    base_stroke_color: constr(pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") = Field("#23445d", description="RGB hexadecimal value") # type: ignore[reportInvalidTypeForm]
-    # base_fill_color: constr(pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") = Field("#bac8d3", description="RGB hexadecimal value") # type: ignore[reportInvalidTypeForm]
+    base_stroke_color: str = Field(
+        "#23445d",
+        description="RGB hexadecimal value",
+        pattern=r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+    )
 
     base_fill_color: str = Field(
         "#bac8d3",
